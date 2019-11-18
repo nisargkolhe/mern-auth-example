97fc4435d91bfbf0249c4459ff77399e6428bc81
Nisarg Kolhe <nisargkolhe@gmail.com>
2019-11-18 13:04:16 -0500

    Added build folder

---
+++ frontend/build/precache-manifest.34408910928a09a1a0f6ff69a04dfd34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e36aa55f0e84c7086c8ef1e1cf20c7c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97ac1dc9dfa842be7db8",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "26120158ad972d8bd0c8",
+    "url": "/static/css/main.b27612e4.chunk.css"
+  },
+  {
+    "revision": "97ac1dc9dfa842be7db8",
+    "url": "/static/js/2.4bb4005b.chunk.js"
+  },
+  {
+    "revision": "26120158ad972d8bd0c8",
+    "url": "/static/js/main.a5f43f4b.chunk.js"
+  },
+  {
+    "revision": "9aabb23bcbd2c9e69d36",
+    "url": "/static/js/runtime-main.baeb1881.js"
+  }
+]);
